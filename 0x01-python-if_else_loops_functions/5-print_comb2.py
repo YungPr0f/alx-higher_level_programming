6ea0f3a2980d30868e386ef8965b71144a864ed4
Yusuf <yusufomotoyosi@gmail.com>
2022-11-29 13:07:52 -0800

    Python - if/else, loops, functions - 5. 00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for num in range(0, 99):
-    print('{} = 0x{:x}'.format(num, num))
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
